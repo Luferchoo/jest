a9a6c5fad5eea075015ab12d0afcbdc385f02230
lufercho <luisfernandogilgareca@gmail.com>
2022-02-15 11:37:13 -0400

    corrección

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,10 @@ const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const firstMult = document.querySelector("#primer");
+const secondMult = document.querySelector("#segundo");
+const formMult = document.querySelector("#multiplicar-form");
+const divMult = document.querySelector("#resultadomult-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,4 +17,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formMult.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber1 = Number.parseInt(firstMult.value);
+  const secondNumber1 = Number.parseInt(secondMult.value);
+
+  divMult.innerHTML = "<p>" + multiplicar(firstNumber1, secondNumber1) + "</p>";
 });
